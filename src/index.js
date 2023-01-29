1844de1e78250ac92dca5325c6dca8f46bbcbd7c
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-29 09:48:51 -0300

    terminando o projeto

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { PersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/mago.js"
 import { Arqueiro } from "./modules/arqueiro.js"
 import { ArqueiroMago } from "./modules/arqueiro-mago.js"
+import { Guerreiro} from "./modules/guerreiro.js"
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
 const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
 const arqueiroMagicoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
+const guerreiroJorge = new Guerreiro('Jorge', 8)
 
-const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagicoChico]
+// arqueiroBruno.#level = 15
+const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagicoChico, guerreiroJorge]
 
 new PersonagemView(personagens).render()
